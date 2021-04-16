fb9b8381c74480ee7995d8a4808927fe00ba93e6
ashkanlarni <ash.larni@gmail.com>
2021-04-16 19:05:44 +0430

    model user added.

--- seproject/models.py
+++ seproject/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=128)
+    mobile = models.CharField(max_length=10)
+    email = models.EmailField()
+    isAdmin = models.BooleanField(default=False)
