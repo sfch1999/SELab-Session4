6ab3bd4d95d514d70dd442435879e3d43874c685
ashkanlarni <ash.larni@gmail.com>
2021-04-16 21:38:17 +0430

    login added.

--- project/urls.py
+++ project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from seproject.views import API, Register
+from seproject.views import API, Register, Login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/main', API.as_view({'post': 'handle_request'})),
     path('api/register', Register.as_view({'post': 'handle_request'})),
+    path('api/login', Login.as_view({'post': 'handle_request'})),
 ]
--- seproject/views.py
+++ seproject/views.py
@@ -1,3 +1,8 @@
+import datetime
+import hashlib
+import random
+import string
+
 import django
 from django.http import HttpResponse
 from rest_framework import viewsets
@@ -8,7 +13,7 @@ import requests
 
 admin = User()
 admin.email, admin.mobile = 'email@email.com', '0000000000'
-admin.username, admin.password = 'admin', str(hash('admin'))
+admin.username, admin.password = 'admin', hashlib.md5('admin'.encode('utf-8')).digest()
 admin.isAdmin = True
 try:
     admin.save()
@@ -25,25 +30,57 @@ class API(viewsets.ViewSet):
             return HttpResponse('Bad Request', status=400)
         if service == 'register':
             return self.register(request.data)
+        if service == 'login':
+            return self.login(request.data)
         return HttpResponse('Bad Request', status=400)
 
     @staticmethod
     def register(data):
         url = 'http://127.0.0.1:8000/api/register'
         response = requests.post(url, data=data)
-        return HttpResponse(response.text,status=response.status_code)
+        return HttpResponse(response.text, status=response.status_code)
+
+    @staticmethod
+    def login(data):
+        url = 'http://127.0.0.1:8000/api/login'
+        response = requests.post(url, data=data)
+        return HttpResponse(response.text, status=response.status_code)
 
 
 class Register(viewsets.ViewSet):
     def handle_request(self, request):
+        user = User()
+        data = request.data
         try:
-            data = request.data
-            user = User()
             user.email, user.mobile = data['email'], data['mobile']
-            user.username, user.password = data['username'], str(hash(data['password']))
-            user.save()
+            user.username, user.password = data['username'], hashlib.md5(data['password'].encode('utf-8')).digest()
+            try:
+                user.save()
+            except django.db.utils.IntegrityError:
+                return HttpResponse('Conflict', status=409)
         except KeyError:
             return HttpResponse('Required fields are empty!!!', status=406)
-        except django.db.utils.IntegrityError:
-            return HttpResponse('Conflict', status=409)
-        return HttpResponse('User created',status=200)
+        return HttpResponse('User created', status=200)
+
+
+class Login(viewsets.ViewSet):
+    def handle_request(self, request):
+        try:
+            data = request.data
+            username, password = data['username'], str(data['password'])
+        except KeyError:
+            return HttpResponse('Required fields are empty!!!', status=406)
+        user = User.objects.get(username=username)
+        print(user.password, hashlib.md5(password.encode('utf-8')).digest())
+        if str(user.password) == str(hashlib.md5(password.encode('utf-8')).digest()):
+            print(user.token_exp_time)
+            print(django.utils.timezone.now())
+            if user.token_exp_time > django.utils.timezone.now():
+                return HttpResponse(user.token, status=200)
+            else:
+                user.token = ''.join(random.choices(string.ascii_uppercase + string.digits, k=100))
+                user.token_exp_time = django.utils.timezone.now() + django.utils.timezone.timedelta(hours=1, minutes=30)
+                user.save()
+                return HttpResponse(user.token, status=200)
+        else:
+            return HttpResponse("Provided Info is wrong!", status=404)
